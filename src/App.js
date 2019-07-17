b82d905c374a7d5c4be8599754d7be6692c86a70
ch04937 <97hernandez.c@gmail.com>
2019-07-17 15:56:06 -0700

    'setting up components'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import ReactDom from 'react-dom'
+
+import NasaGrid from '../src/components/NasaGrid'
+
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+
+  })
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
+
       </p>
+      <NasaGrid limit ={10}  />
+      {/*
+      <NasaGrid limit = {10} column = {3}/>
+        <NasaCards>
+          <img>
+            <a href =''>
+      */}
+
     </div>
   );
 }
